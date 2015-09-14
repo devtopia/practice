d9127255ba003413583de0a76afee935f7da1d07
park-jh <park-jh@gameon.co.jp>
2015-09-15 05:38:35 +0900

    Add the testcase for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,15 @@
+require 'nkf'
+
 class Customer < ActiveRecord::Base
-  validates :family_name, presence: true, length: {maximum: 40}
-  validates :given_name, presence: true, length: {maximum: 40}
+  validates :family_name, presence: true, length: {maximum: 40},
+            format: {with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/, allow_blank: true}
+  validates :given_name, presence: true, length: {maximum: 40},
+            format: {with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/, allow_blank: true}
   validates :family_name_kana, presence: true, length: {maximum: 40}
   validates :given_name_kana, presence: true, length: {maximum: 40}
+
+  before_validation do
+    self.family_name_kana = NKF.nkf('-wh2', family_name_kana) if family_name_kana
+    self.given_name_kana = NKF.nkf('-wh2', given_name_kana) if given_name_kana
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  let(:customer) do
-    Customer.new(
-      family_name: '山田', given_name: '太郎',
-      family_name_kana: 'ヤマダ', given_name_kana: 'タロウ'
-    )
-  end
+  let(:customer) { FactoryGirl.build(:customer) }
 
   example '妥当なオブジェクト' do
     expect(customer).to be_valid
@@ -25,4 +20,28 @@ RSpec.describe Customer, type: :model do
       expect(customer.errors[column_name]).to be_present
     end
   end
+
+  %w{family_name given_name}.each do |column_name|
+    example "#{column_name}は漢字、ひらがな、カタカナを含んでもよい" do
+      customer[column_name] = '亜あア'
+      expect(customer).to be_valid
+    end
+
+    example "#{column_name}は漢字、ひらがな、カタカナ以外の文字を含まない" do
+      ['A', '1', '@'].each do |value|
+        customer[column_name] = value
+        expect(customer).not_to be_valid
+        expect(customer[column_name]).to be_present
+      end
+    end
+  end
+
+  %w(family_name_kana given_name_kana).each do |column_name|
+    example "#{column_name}に含まれるひらがなはカタカナに交換して受け入れる" do
+      customer[column_name] = 'あいう'
+      expect(customer).to be_valid
+      expect(customer[column_name]).to eq('アイウ')
+    end
+  end
+
 end
