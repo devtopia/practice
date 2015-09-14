e8f56aeb938697a1189661325b36fc714f4e84f1
park-jh <park-jh@gameon.co.jp>
2015-09-14 17:45:56 +0900

    Add the testcase for valid customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  example '妥当なオブジェクト' do
+    customer = Customer.new(
+      family_name: '山田', given_name: '太郎',
+      family_name_kana: 'ヤマダ', given_name_kana: 'タロウ'
+    )
+    expect(customer).to be_valid
+  end
+
   %w{family_name given_name family_name_kana given_name_kana}.each do |column_name|
     example "#{column_name}は空であってはならない" do
       customer = Customer.new(
