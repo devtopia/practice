da109345c59d33cae1040456b04c5159668fa1f0
park-jh <park-jh@gameon.co.jp>
2015-09-14 18:06:47 +0900

    Modify to use let method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
-  validates :family_name, presence: true
-  validates :given_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :given_name_kana, presence: true
+  validates :family_name, presence: true, length: {maximum: 40}
+  validates :given_name, presence: true, length: {maximum: 40}
+  validates :family_name_kana, presence: true, length: {maximum: 40}
+  validates :given_name_kana, presence: true, length: {maximum: 40}
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  example '妥当なオブジェクト' do
-    customer = Customer.new(
+  let(:customer) do
+    Customer.new(
       family_name: '山田', given_name: '太郎',
       family_name_kana: 'ヤマダ', given_name_kana: 'タロウ'
     )
+  end
+
+  example '妥当なオブジェクト' do
     expect(customer).to be_valid
   end
 
   %w{family_name given_name family_name_kana given_name_kana}.each do |column_name|
     example "#{column_name}は空であってはならない" do
-      customer = Customer.new(
-        family_name: '山田', given_name: '太郎',
-        family_name_kana: 'ヤマダ', given_name_kana: 'タロウ'
-      )
       customer[column_name] = ''
+      expect(customer).not_to be_valid
+      expect(customer.errors[column_name]).to be_present
+    end
+
+    example "#{column_name}は40文字以内" do
+      customer[column_name] = 'ア' * 41
       expect(customer).not_to be_valid
       expect(customer.errors[column_name]).to be_present
     end
