24911c366b24a05958ab842e0e90d7841e73b98f
park-jh <park-jh@gameon.co.jp>
2015-09-27 14:57:40 +0900

    Modify Customer.authenticate to give a point to user when logged in.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -33,7 +33,19 @@ class Customer < ActiveRecord::Base
   def self.authenticate(username, password)
     customer = find_by_username(username)
     if customer.try(:password_digest) && BCrypt::Password.new(customer.password_digest) == password
-      customer.rewards.create(points: 1)
+      Time.zone = 'Tokyo'
+      now = Time.current
+      if now.hour < 5
+        time0 = now.yesterday.midnight.advance(hours: 5)
+        time1 = now.midnight.advance(hours: 5)
+      else
+        time0 = now.midnight.advance(hours: 5)
+        time1 = now.tomorrow.midnight.advance(hours: 5)
+      end
+
+      unless customer.rewards.where(created_at: time0...time1).exists?
+        customer.rewards.create(points: 1)
+      end
       customer
     else
       nil
